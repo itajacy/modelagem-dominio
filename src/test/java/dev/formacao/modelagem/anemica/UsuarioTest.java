5a180a1d747ee5f7d5b2a92499ce7cea0c835e54
ita Dev <itajacy@gmail.com>
2025-12-26 09:16:24 -0300

    Aula 13 - Cobertura de testes da classe Usuario - usando Maven - mvn test - instalar Maven

--- src/test/java/dev/formacao/modelagem/anemica/UsuarioTest.java
+++ src/test/java/dev/formacao/modelagem/anemica/UsuarioTest.java
@@ -44,4 +44,122 @@ public class UsuarioTest {
         assertEquals("123456", usuario.getSenha());
     }
 
+    @Test
+    void deveCriarUsuariosIguais() {
+        Usuario usuario1 = new Usuario();
+        usuario1.setId("1");
+        usuario1.setNome("João");
+        usuario1.setEmail("joao@zmail.com");
+        usuario1.setSenha("senha123");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId("1");
+        usuario2.setNome("João");
+        usuario2.setEmail("joao@zmail.com");
+        usuario2.setSenha("senha123");
+
+        assertEquals(usuario1, usuario2);
+    }
+
+    @Test
+    void deveCriarUsuariosDiferentes() {
+        Usuario usuario1 = new Usuario();
+        usuario1.setId("1");
+        usuario1.setNome("João");
+        usuario1.setEmail("joao@zmail.com");
+        usuario1.setSenha("senha123");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId("2");
+        usuario2.setNome("Maria");
+        usuario2.setEmail("maria@zmail.com");
+        usuario2.setSenha("senha456");
+
+        assertNotEquals(usuario1, usuario2);
+    }
+
+
+    @Test
+    void deveCompararUsuarioComOutroObjeto() {
+        Usuario usuario = new Usuario();
+        usuario.setId("1");
+        usuario.setNome("João");
+        usuario.setEmail("joao@zmail.com");
+        usuario.setSenha("senha123");
+
+        Object outroObjeto = new Object();
+
+        assertNotEquals(usuario, outroObjeto);
+    }
+
+    @Test
+    void deveCompararUsuarioComNull() {
+        Usuario usuario = new Usuario();
+        usuario.setId("1");
+        usuario.setNome("João");
+        usuario.setEmail("joao@zmail.com");
+        usuario.setSenha("senha123");   
+        assertNotEquals(usuario, null);
+    }
+
+    @Test
+    void deveCompararUsuarioComEleMesmo() {
+        Usuario usuario = new Usuario();
+        usuario.setId("1");
+        usuario.setNome("João");
+        usuario.setEmail("joao@zmail.com");
+        usuario.setSenha("senha123");
+
+        Usuario usuario2 = usuario;
+
+        assertEquals(usuario, usuario2);
+    }
+
+    @Test
+    void deveCompararUsuarioComIdNullComUsuario() {
+        Usuario usuario1 = new Usuario();
+        usuario1.setId(null);
+        usuario1.setNome("João");
+        usuario1.setEmail("joao@zmail.com");
+        usuario1.setSenha("senha123");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId("2");
+        usuario2.setNome("João");
+        usuario2.setEmail("joao@zmail.com");
+        usuario2.setSenha("senha123");
+
+        assertNotEquals(usuario1, usuario2);
+    }
+
+    @Test
+    void deveCompararUsuarioComIdNullComOutroUsuarioIdNull() {
+        Usuario usuario1 = new Usuario();
+        usuario1.setId(null);
+        usuario1.setNome("João");
+        usuario1.setEmail("joao@zmail.com");
+        usuario1.setSenha("senha123");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(null);
+        usuario2.setNome("João");
+        usuario2.setEmail("joao@zmail.com");
+        usuario2.setSenha("senha123");
+
+        assertEquals(usuario1, usuario2);
+    }
+
+    @Test
+    void deveGerarHashCodeCorreto() {
+        Usuario usuario = new Usuario();
+        usuario.setId("1");
+        usuario.setNome("João");
+        usuario.setEmail("joao@zmail.com");
+        usuario.setSenha("senha123");
+
+        int expectedHashCode = usuario.getId().hashCode();
+        assertEquals(expectedHashCode, usuario.hashCode());
+
+    }
+
 }
