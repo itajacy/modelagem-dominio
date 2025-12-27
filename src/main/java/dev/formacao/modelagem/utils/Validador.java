e5a806d3a02267c148efe990888bd682ae24343a
ita Dev <itajacy@gmail.com>
2025-12-27 09:42:13 -0300

    Aula 15 - Modelo Rico - Validador #1 - import static org.junit.jupiter.api.Assertions.*;

--- src/main/java/dev/formacao/modelagem/utils/Validador.java
+++ src/main/java/dev/formacao/modelagem/utils/Validador.java
@@ -1,5 +1,17 @@
 package dev.formacao.modelagem.utils;
 
 public class Validador {
-    
+
+    private Validador() {
+    }
+
+    public static String naoNulo(Object objeto, String msgErro) {
+        return objeto != null ? null : msgErro;
+    }
+    public static String naoVazio(String valor, String erro) {
+        if (naoNulo(valor, erro) != null) {
+            return erro;
+        }
+        return !valor.trim().isEmpty() ? null : erro;
+    }
 }
--- src/test/java/dev/formacao/modelagem/utils/ValidadorTest.java
+++ src/test/java/dev/formacao/modelagem/utils/ValidadorTest.java
@@ -1,5 +1,27 @@
 package dev.formacao.modelagem.utils;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class ValidadorTest {
-    
+
+    private static final String MSG_ERRO = "Erro genérico";
+
+    @Test
+    void deveRetornarNuloQuandoObjetoForValido() {
+        assertNull(Validador.naoNulo("ABC",  MSG_ERRO));
+        assertNull(Validador.naoNulo("123",  MSG_ERRO));
+        assertNull(Validador.naoNulo(new Object(),  MSG_ERRO));
+        // String resultado = Validador.naoNulo("objeto valido", "Objeto nulo");
+        // assert resultado == null;
+
+    }
+
+    @Test
+    void deveRetornarMensagemQuandoObjetoForNulo() {
+       assertEquals(Validador.naoNulo(null, MSG_ERRO), MSG_ERRO);
+    }
+
 }
