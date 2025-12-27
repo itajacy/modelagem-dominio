1c098e32ac3da83fe4ff2095a666a18a64c64264
ita Dev <itajacy@gmail.com>
2025-12-27 17:39:46 -0300

    Aula 21 - Modelo Rico - Email - criado objeto de valor Email em  \modelagem\shared\Email.java

--- src/main/java/dev/formacao/modelagem/auth/usuario/Usuario.java
+++ src/main/java/dev/formacao/modelagem/auth/usuario/Usuario.java
@@ -2,9 +2,13 @@ package dev.formacao.modelagem.auth.usuario;
 
 import java.time.Instant;
 
+import dev.formacao.modelagem.shared.Email;
+
 public class Usuario {
     private String id;
     private String nome;
-    private String email;
+    private Email email; // usando a classe objeto de valor Email
     private Instant dataCadastro;
 }
+
+
