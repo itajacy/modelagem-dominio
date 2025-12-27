5ac9eb8e834673e33437908cac8ad44599d08f45
ita Dev <itajacy@gmail.com>
2025-12-27 16:57:24 -0300

    Aula 20 - Modelo Rico - Roadmap, transformando modelo anemico em modelo rico

---
+++ src/main/java/dev/formacao/modelagem/auth/usuario/Usuario.java
@@ -0,0 +1,10 @@
+package dev.formacao.modelagem.auth.usuario;
+
+import java.time.Instant;
+
+public class Usuario {
+    private String id;
+    private String nome;
+    private String email;
+    private Instant dataCadastro;
+}
